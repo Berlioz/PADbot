919d8dbaed690c91a5c443e3b34d6eeb089ffabd
Victor Fan <vfan@blurb.com>
2014-01-30 17:53:09 -0800

    WIP for awakening refactor

---
+++ models/awakening.rb
@@ -0,0 +1,23 @@
+class Awakening
+  include DataMapper::Resource
+
+  property :id, Integer, :key => true
+  property :name, String
+  property :effect, String
+
+  BASE_URL = "http://www.puzzledragonx.com/en/awokenskill.asp?s="
+
+  def self.lookup(id)
+    self.first(:id => id) || self.scrape(id)
+  end
+
+  def self.scrape(id)
+    unless self.first(:id => id)
+      @doc = Nokogiri::HTML(open(BASE_URL + id.to_s))
+      lines = @doc.xpath("//td[@class='value-end']")
+      name = lines.first.children.first.to_s
+      effect = lines.last.children.first.to_s
+      self.create!(:id => id, :name => name, :efffect => effect)
+    end  
+  end
+end
