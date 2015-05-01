45eb6c7717d096053fea2e8d94cec03f8426037b
Ubuntu <ubuntu@ip-10-252-102-37.us-west-2.compute.internal>
2015-05-01 22:21:03 +0000

    typo fix in the new apropos plugin

--- plugins/apropos.rb
+++ plugins/apropos.rb
@@ -7,14 +7,20 @@ class AproposPlugin < PazudoraPluginBase
 "!pad which [NAME]: Returns a set of all monster names which contain NAME as a substring."
   end
 
+  def initialize
+    @names = Monster.all.map{|m| [m.name, m.id]}
+    p @names
+  end
+
   def respond(m, args)
+    p "debug: enterming apropos with #{args}"
     matches = substring_search(args)
     if matches.length > 8
-      m.reply("More than 8 results were found; please narrow your query.")
+      m.reply("More than 10 results were found; please narrow your query.")
     elsif matches.length == 0
       m.reply("No matches found for substring '#{args}'")
     else
-      r = "Matches found: " + matches.map{|monster| format(monster)}.join(", ")
+      m.reply "Matches found: " + matches.map{|monster| format(monster)}.join(", ")
     end
   end
 
@@ -23,6 +29,7 @@ class AproposPlugin < PazudoraPluginBase
   end
 
   def substring_search(identifier)
-    Monster.all.select{|m| m.name.downcase.include?(identifier.downcase) }
+    names = @names.select{|m| m.first.downcase.include?(identifier.downcase) }
+    names.map{|name, id| Monster.get(id)}
   end
 end
